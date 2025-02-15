0ec6690b9f7e3a6d167e7a9451516815efc83a58
BVNgocAnh <vietanh05092001@gmail.com>
2025-02-16 00:17:40 +0700

    chapter-3-action-radio

--- chapter-3.py
+++ chapter-3.py
@@ -1,5 +1,5 @@
 import asyncio
-from playwright.async_api import async_playwright, Playwright
+from playwright.async_api import async_playwright, Playwright, expect
 
 async def action_textbox(playwright: Playwright):
     browser = await playwright.chromium.launch(headless=False, slow_mo=600)
@@ -21,21 +21,32 @@ async def action_textbox(playwright: Playwright):
 async def action_checkbox(playwright: Playwright):
     browser = await playwright.chromium.launch(headless=False, slow_mo=600)
     page = await browser.new_page()
-    await page.goto("https://demoqa.com/checkbox")
+    await page.goto("https://demoqa.com/checkbox", wait_until="domcontentloaded")
 
     folder_checkbox = page.locator(".rct-checkbox")
-    await folder_checkbox.click()
     await folder_checkbox.highlight()
-
     await folder_checkbox.click()
 
     await asyncio.sleep(7)
 
 
+async def action_radio(playwright: Playwright):
+    browser = await playwright.chromium.launch(headless=False, slow_mo=600)
+    page = await browser.new_page()
+    await page.goto("https://demoqa.com/radio-button", wait_until="domcontentloaded")
 
+    yes_radio = page.locator("//label[@for='yesRadio']")
+    await yes_radio.highlight()
+    await yes_radio.check()
+    assert (await yes_radio.is_checked())
+
+
+    await expect(page.locator("//label[@for='noRadio']")).not_to_be_checked()
+    
+    await asyncio.sleep(20)
 
 async def main():
     async with async_playwright() as playwright:
-        await action_checkbox(playwright)
+        await action_radio(playwright)
 
 asyncio.run(main())
