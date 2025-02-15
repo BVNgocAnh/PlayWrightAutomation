aa0be4cf8284633c28cfab6d8d21fa72b4d8f587
BVNgocAnh <vietanh05092001@gmail.com>
2025-02-15 18:25:42 +0700

    Action: text_box

---
+++ chapter-3.py
@@ -0,0 +1,24 @@
+import asyncio
+from playwright.async_api import async_playwright, Playwright
+
+async def action_textbox(playwright: Playwright):
+    browser = await playwright.chromium.launch(headless=False, slow_mo=600)
+    page = await browser.new_page()
+    await page.goto("https://demoqa.com/text-box")
+
+    fullname_textbox = page.locator("#userName")
+    await fullname_textbox.fill("AutomationTesting")
+
+    email_textbox = page.locator("#userEmail")
+    await email_textbox.fill("Testing@gmail.com")
+
+    currentAddress_textbox = page.locator("#currentAddress")
+    await currentAddress_textbox.fill("Tran Van Hoai")
+    await currentAddress_textbox.clear()
+    await asyncio.sleep(7)
+
+async def main():
+    async with async_playwright() as playwright:
+        await action_textbox(playwright)
+
+asyncio.run(main())
